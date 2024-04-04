574066dad017d002d7281edb264277ec81b0aa16
Joao <joaomoreira@agger.com.br>
2024-04-04 10:42:13 -0300

    Ajuste de informações do abrigo

--- src/shelter/usecases/get.shelter.details.usecase.ts
+++ src/shelter/usecases/get.shelter.details.usecase.ts
@@ -1,15 +1,19 @@
-import { IUseCase } from "src/domain/iusecase.interface";
-import GetShelterDetailsUseCaseOutput from "./dtos/get.shelter.details.usecase.output";
+import { IUseCase } from 'src/domain/iusecase.interface';
+import GetShelterDetailsUseCaseOutput from './dtos/get.shelter.details.usecase.output';
 
-export default class GetShelterDetailsUseCase implements IUseCase<null, GetShelterDetailsUseCaseOutput> {
-    run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
-        return Promise.resolve(new GetShelterDetailsUseCaseOutput({
-            shelterName: 'Abrigo Bigo',
-            shelterEmail: 'abb@gmail.com',
-            shelterPhone: '19981281830',
-            shelterWhatsApp: '199818182819',
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }))
-    }
+export default class GetShelterDetailsUseCase
+  implements IUseCase<null, GetShelterDetailsUseCaseOutput>
+{
+  run(input: null): Promise<GetShelterDetailsUseCaseOutput> {
+    return Promise.resolve(
+      new GetShelterDetailsUseCaseOutput({
+        shelterName: 'Anjos de Focinho',
+        shelterEmail: 'anjosdefc@gmail.com',
+        shelterPhone: '19982384830',
+        shelterWhatsApp: '199818183520',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }),
+    );
+  }
 }
