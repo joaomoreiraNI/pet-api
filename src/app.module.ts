c3b25b83ad2a8356ab1c965af077ee1307188d2a
Joao <joaomoreira@agger.com.br>
2024-04-04 22:33:49 -0300

    Crianção conexão com o MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShelterModule } from './shelter/shelter.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ShelterModule],
+  imports: [
+    ShelterModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://victorrjoao:hgdasdhvwqdiJ17$@pet-api-cluster.cx6jwfn.mongodb.net/pet-db?retryWrites=true&w=majority&appName=pet-api-cluster',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
